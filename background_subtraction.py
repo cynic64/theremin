193f00e2bf7534753203435a9c47219c7081311c
Steve Larsen <f3bl@gmx.com>
2018-09-18 08:06:26 +0200

    Background subtraction is a bit better

--- background_subtraction.py
+++ background_subtraction.py
@@ -1,59 +1,29 @@
-'''
-import numpy as np
-import cv2
-
-# local module
-import video
-
-camera = video.create_capture(0, video.presets['cube'])
-
-fgbg = cv2.bgsegm.createBackgroundSubtractorGMG()
-
-while(1):
-    ret, frame = camera.read()
-
-    fgmask = fgbg.apply(frame)
-    fgmask = cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)
-    frame &= fgmask
-
-    cv2.imshow('frame', frame)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
-
-camera.release()
-cv2.destroyAllWindows()
-'''
-
 import numpy as np
 import cv2
 
 
 class BackGroundSubtractor:
-    # When constructing background subtractor, we
-    # take in two arguments:
-    # 1) alpha: The background learning factor, its value should
-    # be between 0 and 1. The higher the value, the more quickly
-    # your program learns the changes in the background. Therefore, 
-    # for a static background use a lower value, like 0.001. But if 
-    # your background has moving trees and stuff, use a higher value,
-    # maybe start with 0.01.
-    # 2) firstFrame: This is the first frame from the video/webcam.
-    def __init__(self,alpha,firstFrame):
-        self.alpha  = alpha
+    def __init__(self, firstFrame):
+        # by default, uses only the first frame
+        # to compute a background
+        self.avg_frames = 1
+        self.alpha = 1 / self.avg_frames
         self.backGroundModel = firstFrame
+        self.updates = 0
 
-    def getForeground(self,frame):
-        # apply the background averaging formula:
-        # NEW_BACKGROUND = CURRENT_FRAME * ALPHA + OLD_BACKGROUND * (1 - APLHA)
-        # self.backGroundModel =  frame * self.alpha + self.backGroundModel * (1 - self.alpha)
+    def getForeground(self, frame):
+        if self.updates < self.avg_frames:
+            # apply the background averaging formula:
+            # NEW_BACKGROUND = CURRENT_FRAME * ALPHA + OLD_BACKGROUND * (1 - APLHA)
+            self.backGroundModel =  frame * self.alpha + self.backGroundModel * (1 - self.alpha)
+
+        self.updates += 1
 
         # after the previous operation, the dtype of
         # self.backGroundModel will be changed to a float type
         # therefore we do not pass it to cv2.absdiff directly,
         # instead we acquire a copy of it in the uint8 dtype
         # and pass that to absdiff.
-
         return cv2.absdiff(self.backGroundModel.astype(np.uint8),frame)
 
 cam = cv2.VideoCapture(0)
@@ -68,7 +38,7 @@ def denoise(frame):
 
 ret, frame = cam.read()
 if ret is True:
-    backSubtractor = BackGroundSubtractor(0.01,denoise(frame))
+    backSubtractor = BackGroundSubtractor(denoise(frame))
     run = True
 else:
     run = False
@@ -84,10 +54,12 @@ while(run):
 
         # get the foreground
         foreGround = backSubtractor.getForeground(denoise(frame))
+        foreGround = cv2.cvtColor(foreGround, cv2.COLOR_BGR2GRAY)
 
         # Apply thresholding on the background and display the resulting mask
-        ret, mask = cv2.threshold(foreGround, 15, 255, cv2.THRESH_BINARY)
-        # mask = cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)
+        mask = foreGround
+        ret, mask = cv2.threshold(foreGround, 50, 255, cv2.THRESH_BINARY)
+        mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
         frame &= mask
 
         # Note: The mask is displayed as a RGB image, you can
