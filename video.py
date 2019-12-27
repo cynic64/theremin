07976f10501a07c811b86fbebc9816bcb856257c
cynic64 <f3bl@gmx.com>
2019-12-27 14:05:08 +0100

    updated video.py to work wtih python 3.8

--- video.py
+++ video.py
@@ -38,7 +38,7 @@ from numpy import pi, sin, cos
 import cv2 as cv
 
 # built-in modules
-from time import clock
+from time import process_time as clock
 
 # local modules
 from tst_scene_render import TestSceneRender
